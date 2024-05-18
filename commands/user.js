446e2380a344fe67c5d45623b91ab4eba1ff3af8
Andrew Bashore <andrew@bashtech.net>
2024-05-18 19:52:37 -0400

    Add confirmations for kick and ban commnands.

--- commands/user.js
+++ commands/user.js
@@ -1,7 +1,10 @@
 /* jshint esversion: 11 */
 
 const {
-	SlashCommandBuilder
+	SlashCommandBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle
 } = require('discord.js');
 const config = require("../config/aod-discord-bot.config.json");
 
@@ -26,10 +29,11 @@ module.exports = {
 		.addSubcommand(command => command.setName('clearptt').setDescription('Removes the PTT role from a user')
 			.addUserOption(option => option.setName('user').setDescription('User').setRequired(true))),
 	help: true,
-	checkPerm(perm, commandName, parentName) {
+	checkPerm(perm, commandName) {
 		switch (commandName) {
+			case 'user':
 			case 'kick':
-				return perm >= global.PERM_RECRUITER
+				return perm >= global.PERM_RECRUITER;
 			case 'ban':
 			case 'mute':
 			case 'unmute':
@@ -38,12 +42,6 @@ module.exports = {
 				return perm >= global.PERM_MOD;
 		}
 		return false;
-	},
-	async autocomplete(interaction, member, perm, permName) {
-		const subCommand = interaction.options.getSubcommand();
-		const focusedOption = interaction.options.getFocused(true);
-		let search = focusedOption.value.toLowerCase();
-		switch (subCommand) {}
 	},
 	async execute(interaction, member, perm, permName) {
 		const subCommand = interaction.options.getSubcommand();
@@ -63,9 +61,42 @@ module.exports = {
 
 				let reason = interaction.options.getString('reason') ?? "No reason provided";
 
-				targetMember.kick(`Requested by ${global.getNameFromMessage(interaction)}: ${reason}`)
-					.catch(error => interaction.reply({ content: `Sorry, I couldn't kick because of : ${error}`, ephemeral: true }));
-				return interaction.reply({ content: `${targetMember.user.username} has been kicked by ${member.user} because: ${reason}`, ephemeral: true });
+				const confirm = new ButtonBuilder()
+					.setCustomId('confirm_user_kick')
+					.setLabel('Confirm Kick')
+					.setStyle(ButtonStyle.Danger);
+				const cancel = new ButtonBuilder()
+					.setCustomId('cancel_user_kick')
+					.setLabel('Cancel')
+					.setStyle(ButtonStyle.Secondary);
+				const row = new ActionRowBuilder()
+					.addComponents(cancel, confirm);
+				const response = await interaction.reply({
+					content: `Are you sure you want to kick ${targetMember} from the server?`,
+					components: [row],
+					ephemeral: true
+				});
+
+				const filter = (i) => (i.customId === 'confirm_user_kick' || i.customId === 'cancel_user_kick') && i.user.id === interaction.user.id;
+				try {
+					const confirmation = await response.awaitMessageComponent({ filter: filter, time: 10000 });
+					if (confirmation.customId === 'confirm_user_kick') {
+						await targetMember.kick(`Requested by ${global.getNameFromMessage(interaction)}: ${reason}`)
+							.catch(error => interaction.reply({ content: `Sorry, I couldn't kick because of : ${error}`, ephemeral: true }));
+						await confirmation.update({
+							content: `${targetMember} has been kicked for: ${reason}`,
+							components: []
+						}).catch(() => {});
+					} else if (confirmation.customId === 'cancel_user_kick') {
+						await confirmation.update({
+							content: 'Cancelled',
+							components: []
+						});
+					}
+				} catch (e) {
+					await interaction.editReply({ content: 'Timeout waiting for confirmation', components: [], ephemeral: true });
+				}
+				break;
 			}
 			case 'ban': {
 				let userToBan;
@@ -87,9 +118,42 @@ module.exports = {
 				let reason = interaction.options.getString('reason') ?? "No reason provided";
 				let purgeDuration = interaction.options.getInteger('delete-messages') ?? 0
 
-				interaction.guild.members.ban(userToBan, { reason: `Requested by ${global.getNameFromMessage(interaction)}: ${reason}`, deleteMessageSeconds: purgeDuration })
-					.catch(error => interaction.reply({ content: `Sorry, I couldn't ban because of : ${error}`, ephemeral: true }));
-				return interaction.reply({ content: `${userToBan.username} has been banned by ${member.user} because: ${reason}`, ephemeral: true });
+				const confirm = new ButtonBuilder()
+					.setCustomId('confirm_user_ban')
+					.setLabel('Confirm Ban')
+					.setStyle(ButtonStyle.Danger);
+				const cancel = new ButtonBuilder()
+					.setCustomId('cancel_user_ban')
+					.setLabel('Cancel')
+					.setStyle(ButtonStyle.Secondary);
+				const row = new ActionRowBuilder()
+					.addComponents(cancel, confirm);
+				const response = await interaction.reply({
+					content: `Are you sure you want to ban ${userToBan} from the server?`,
+					components: [row],
+					ephemeral: true
+				});
+
+				const filter = (i) => (i.customId === 'confirm_user_ban' || i.customId === 'cancel_user_ban') && i.user.id === interaction.user.id;
+				try {
+					const confirmation = await response.awaitMessageComponent({ filter: filter, time: 10000 });
+					if (confirmation.customId === 'confirm_user_ban') {
+						await interaction.guild.members.ban(userToBan, { reason: `Requested by ${global.getNameFromMessage(interaction)}: ${reason}`, deleteMessageSeconds: purgeDuration })
+							.catch(error => interaction.reply({ content: `Sorry, I couldn't ban because of : ${error}`, ephemeral: true }));
+						await confirmation.update({
+							content: `${userToBan} has been banned for: ${reason}`,
+							components: []
+						}).catch(() => {});
+					} else if (confirmation.customId === 'cancel_user_ban') {
+						await confirmation.update({
+							content: 'Cancelled',
+							components: []
+						});
+					}
+				} catch (e) {
+					await interaction.editReply({ content: 'Timeout waiting for confirmation', components: [], ephemeral: true });
+				}
+				break;
 			}
 			case 'mute':
 			case 'unmute': {
