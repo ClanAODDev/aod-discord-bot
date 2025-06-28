1765f00f64222ce80d472e72a61bd35e05dbe08f
Adam Schultz <adam.schultz@live.com>
2025-06-27 22:43:50 -0400

    Fix donate typo

--- commands/donate.js
+++ commands/donate.js
@@ -10,8 +10,8 @@ module.exports = {
 	async execute(interaction) {
 		return messageReply(interaction, {
 			"id": 652627557,
-			"title": "ClanAOD Stack Up Donation Drive",
-			"description": "ClanAOD is proud to support Stack Up, a charity dedicated to supporting veterans and active-duty military through the power of gaming. We’re raising funds to help provide mental health support, supply crates, and community programs that make a real difference. Join us in making an impact—every donation counts!",
+			"title": "ClanAOD Stack Up Donation Drive [Click to Donate]",
+			"description": "ClanAOD is proud to support Stack Up, a charity dedicated to supporting veterans and active-duty military through the power of gaming. We're raising funds to help provide mental health support, supply crates, and community programs that make a real difference. Join us in making an impact--every donation counts!",
 			"color": 2326507,
 			"fields": [],
 			"image": {
