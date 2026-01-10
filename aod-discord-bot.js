f27de9590e4d067f9844930193b3c6e63e7ad69f
Chris Deaton <dcdeaton@ncsu.edu>
2026-01-10 16:44:13 -0500

    Move twitch config to object

--- aod-discord-bot.js
+++ aod-discord-bot.js
@@ -4575,6 +4575,7 @@ var twitchMonitorTimer = null;
 var twitchAccessToken = null;
 var twitchTokenExpiry = 0;
 var twitchStreamWasLive = false;
+var twitchConfig = null;
 
 function forumSyncTimerCallback() {
 	global.lastForumSync = new Date();
@@ -4606,7 +4607,7 @@ function forumSyncTimerCallback() {
 }
 
 async function getTwitchAccessToken() {
-	if (!config.twitchClientId || !config.twitchClientSecret)
+	if (!twitchConfig)
 		return null;
 
 	let now = Date.now();
@@ -4617,7 +4618,7 @@ async function getTwitchAccessToken() {
 		let response = await fetchTimeout('https://id.twitch.tv/oauth2/token', 5000, {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-			body: `client_id=${config.twitchClientId}&client_secret=${config.twitchClientSecret}&grant_type=client_credentials`
+			body: `client_id=${twitchConfig.clientId}&client_secret=${twitchConfig.clientSecret}&grant_type=client_credentials`
 		});
 		let data = await response.json();
 		if (data.access_token) {
@@ -4632,7 +4633,7 @@ async function getTwitchAccessToken() {
 }
 
 async function twitchMonitorCallback() {
-	if (!config.twitchChannelName || !config.twitchClientId)
+	if (!twitchConfig)
 		return;
 
 	let token = await getTwitchAccessToken();
@@ -4640,10 +4641,10 @@ async function twitchMonitorCallback() {
 		return;
 
 	try {
-		let response = await fetchTimeout(`https://api.twitch.tv/helix/streams?user_login=${config.twitchChannelName}`, 5000, {
+		let response = await fetchTimeout(`https://api.twitch.tv/helix/streams?user_login=${twitchConfig.channelName}`, 5000, {
 			method: 'GET',
 			headers: {
-				'Client-ID': config.twitchClientId,
+				'Client-ID': twitchConfig.clientId,
 				'Authorization': `Bearer ${token}`
 			}
 		});
@@ -4653,13 +4654,13 @@ async function twitchMonitorCallback() {
 		if (isLive && !twitchStreamWasLive) {
 			let stream = data.data[0];
 			const guild = client.guilds.resolve(config.guildId);
-			const channel = guild.channels.cache.find(c => c.name === config.twitchNotificationChannel);
+			const channel = guild.channels.cache.find(c => c.name === twitchConfig.notificationChannel);
 			if (channel) {
 				let thumbnailUrl = stream.thumbnail_url.replace('{width}', '400').replace('{height}', '225');
 				let embed = {
 					color: 0x9146FF,
 					title: stream.title,
-					url: `https://twitch.tv/${config.twitchChannelName}`,
+					url: `https://twitch.tv/${twitchConfig.channelName}`,
 					author: { name: `${stream.user_name} is now live on Twitch!` },
 					fields: [{ name: 'Game', value: stream.game_name || 'Unknown', inline: true }],
 					image: { url: `${thumbnailUrl}?t=${Date.now()}` },
@@ -4740,9 +4741,16 @@ client.on('clientReady', async function() {
 	forumSyncTimerCallback(); //prime the date and do initial adds
 	forumSyncTimer = setInterval(forumSyncTimerCallback, config.forumSyncIntervalMS);
 
-	if (config.twitchChannelName && config.twitchClientId) {
+	if (config.twitchClientId && config.twitchClientSecret && config.twitchChannelName) {
+		twitchConfig = {
+			clientId: config.twitchClientId,
+			clientSecret: config.twitchClientSecret,
+			channelName: config.twitchChannelName,
+			notificationChannel: config.twitchNotificationChannel,
+			checkIntervalMS: config.twitchCheckIntervalMS || 60000
+		};
 		twitchMonitorCallback();
-		twitchMonitorTimer = setInterval(twitchMonitorCallback, config.twitchCheckIntervalMS || 60000);
+		twitchMonitorTimer = setInterval(twitchMonitorCallback, twitchConfig.checkIntervalMS);
 	}
 
 	startNextSavedTimer();
